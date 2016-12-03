2b6d65edc2a147c87e47036509605da7212008c3
Punk <jump.e.r@yandex.ru>
2016-12-04 02:30:41 +0300

    tests

--- tests/DeCaptchaErrorsTest.php
+++ tests/DeCaptchaErrorsTest.php
@@ -11,4 +11,24 @@ class DeCaptchaErrorsTest extends PHPUnit_Framework_TestCase
     {
         throw new \jumper423\decaptcha\core\DeCaptchaErrors('ERROR_NO_SUCH_METHOD', null, 'ru');
     }
+
+    /**
+     * @expectedException \jumper423\decaptcha\core\DeCaptchaErrors
+     * @expectedExceptionMessage Ваша капча имеет размер более 100 килобайт: вес файла 5 МБ
+     * @expectedExceptionCode 2
+     */
+    public function testErrorTooBigCaptchaFilesize()
+    {
+        throw new \jumper423\decaptcha\core\DeCaptchaErrors('ERROR_TOO_BIG_CAPTCHA_FILESIZE', 'вес файла 5 МБ', 'ru');
+    }
+
+    /**
+     * @expectedException \jumper423\decaptcha\core\DeCaptchaErrors
+     * @expectedExceptionMessage NOT_CONFIG
+     * @expectedExceptionCode 0
+     */
+    public function testNotConfig()
+    {
+        throw new \jumper423\decaptcha\core\DeCaptchaErrors('NOT_CONFIG');
+    }
 }
