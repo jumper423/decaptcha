d81d1e2c562eec1a3735e8d5259c0580803ea891
jumper423 <jump.e.r@yandex.ru>
2016-12-20 16:51:24 +0000

    Apply fixes from StyleCI

--- tests/RucaptchaTest.php
+++ tests/RucaptchaTest.php
@@ -10,7 +10,7 @@ class RucaptchaTest extends PHPUnit_Framework_TestCase
         $re = new \jumper423\decaptcha\services\TwoCaptcha([
             \jumper423\decaptcha\services\TwoCaptcha::PARAM_SPEC_KEY => 'b7e7f1002f0f917ca5a852e1c0312e8f',
         ]);
-        if ($re->recognize(__DIR__.'/data/Captcha.jpg')){
+        if ($re->recognize(__DIR__.'/data/Captcha.jpg')) {
             $this->assertEquals('11111111111111', $re->getCode());
         } else {
             $this->assertEquals('22222222222222', $re->getError());
