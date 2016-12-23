0bc77ac2b5d65c1fddd8d88493e9e4b0d68991e4
Vlad Pasechnik <pasechnik@playform.com>
2016-12-23 17:14:38 +0300

    RuCaptchaGrid

---
+++ src/services/RuCaptchaGrid.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace jumper423\decaptcha\services;
+
+/**
+ * Class RuCaptchaGrid
+ * @package jumper423\decaptcha\services
+ */
+class RuCaptchaGrid extends RuCaptcha
+{
+    const ACTION_FIELD_RECAPTCHA = 17;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->paramsNames[static::ACTION_FIELD_RECAPTCHA] = 'recaptcha';
+
+        $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_RECAPTCHA] = [
+            static::PARAM_SLUG_DEFAULT => 1,
+            static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_INTEGER,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getCode()
+    {
+        $code = parent::getCode();
+        $code = explode(':', $code)[1];
+        return explode('/', $code);
+    }
+}
