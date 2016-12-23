d8c9c8fdd52de35bbfde7e64f872208dba66c33c
Vlad Pasechnik <pasechnik@playform.com>
2016-12-23 17:25:20 +0300

    RuCaptchaClick

---
+++ src/services/RuCaptchaClick.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace jumper423\decaptcha\services;
+
+/**
+ * Class RuCaptchaGrid
+ * @package jumper423\decaptcha\services
+ */
+class RuCaptchaClick extends RuCaptcha
+{
+    const ACTION_FIELD_COORDINATESCAPTCHA = 18;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->paramsNames[static::ACTION_FIELD_COORDINATESCAPTCHA] = 'coordinatescaptcha';
+
+        $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_COORDINATESCAPTCHA] = [
+            static::PARAM_SLUG_DEFAULT => 1,
+            static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_INTEGER,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getCode()
+    {
+        $code = parent::getCode();
+        $code = explode(':', $code)[1];
+        $code = explode(';', $code);
+        $result = [];
+        foreach ($code as $row) {
+            $rowCoord = explode(',', $row);
+            foreach ($rowCoord as &$rowCoordOne) {
+                $rowCoordOne = substr($rowCoordOne, 2);
+            }
+            $result[] = $rowCoord;
+        }
+        return $result;
+    }
+}
