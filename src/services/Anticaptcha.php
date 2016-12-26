40d6e06f54b015f29308a6ad295ae7981080d353
jumper423 <jump.e.r@yandex.ru>
2016-12-26 21:55:06 +0000

    Apply fixes from StyleCI

--- src/services/Anticaptcha.php
+++ src/services/Anticaptcha.php
@@ -5,8 +5,7 @@ namespace jumper423\decaptcha\services;
 use jumper423\decaptcha\core\DeCaptchaBase;
 
 /**
- * Class Anticaptcha
- * @package jumper423\decaptcha\services
+ * Class Anticaptcha.
  */
 class Anticaptcha extends DeCaptchaBase
 {
@@ -19,19 +18,18 @@ class Anticaptcha extends DeCaptchaBase
     const PARAM_FIELD_TYPE_BOOLEAN = 4;
 
     protected $paramsNames = [
-        self::ACTION_FIELD_KEY => 'clientKey',
-        self::ACTION_FIELD_SOFT_ID => 'softId',
+        self::ACTION_FIELD_KEY      => 'clientKey',
+        self::ACTION_FIELD_SOFT_ID  => 'softId',
         self::ACTION_FIELD_LANGUAGE => 'languagePool',
-        self::ACTION_FIELD_TASK => 'task',
-        self::ACTION_FIELD_METHOD => 'type',
-        self::ACTION_FIELD_FILE => 'body',
-        self::ACTION_FIELD_PHRASE => 'phrase',
+        self::ACTION_FIELD_TASK     => 'task',
+        self::ACTION_FIELD_METHOD   => 'type',
+        self::ACTION_FIELD_FILE     => 'body',
+        self::ACTION_FIELD_PHRASE   => 'phrase',
         self::ACTION_FIELD_REGSENSE => 'case',
-        self::ACTION_FIELD_NUMERIC => 'numeric',
-        self::ACTION_FIELD_CALC => 'math',
-        self::ACTION_FIELD_MIN_LEN => 'minLength',
-        self::ACTION_FIELD_MAX_LEN => 'maxLength',
-
+        self::ACTION_FIELD_NUMERIC  => 'numeric',
+        self::ACTION_FIELD_CALC     => 'math',
+        self::ACTION_FIELD_MIN_LEN  => 'minLength',
+        self::ACTION_FIELD_MAX_LEN  => 'maxLength',
 
 //        self::ACTION_FIELD_METHOD           => 'method',
 //        self::ACTION_FIELD_LANGUAGE         => 'language',
@@ -48,36 +46,36 @@ class Anticaptcha extends DeCaptchaBase
         parent::init();
 
         $this->actions[static::ACTION_RECOGNIZE] = [
-            static::ACTION_URI => 'createTask',
+            static::ACTION_URI    => 'createTask',
             static::ACTION_METHOD => static::ACTION_METHOD_POST,
             static::ACTION_FIELDS => [
                 static::ACTION_FIELD_KEY => [
                     static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_SPEC => static::PARAM_SPEC_API_KEY,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                    static::PARAM_SLUG_SPEC    => static::PARAM_SPEC_API_KEY,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                 ],
                 static::ACTION_FIELD_SOFT_ID => [
                     static::PARAM_SLUG_VARIABLE => false,
-                    static::PARAM_SLUG_DEFAULT => 882,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_INTEGER,
+                    static::PARAM_SLUG_DEFAULT  => 882,
+                    static::PARAM_SLUG_TYPE     => static::PARAM_FIELD_TYPE_INTEGER,
                 ],
                 static::ACTION_FIELD_LANGUAGE => [
                     static::PARAM_SLUG_DEFAULT => 'en',
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                 ],
                 static::ACTION_FIELD_TASK => [
                     static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_OBJECT,
-                    static::ACTION_FIELDS => [
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_OBJECT,
+                    static::ACTION_FIELDS      => [
                         static::ACTION_FIELD_METHOD => [
                             static::PARAM_SLUG_DEFAULT => 'ImageToTextTask',
                             static::PARAM_SLUG_REQUIRE => true,
-                            static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                            static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                         ],
                         static::ACTION_FIELD_FILE => [
                             static::PARAM_SLUG_REQUIRE => true,
-                            static::PARAM_SLUG_SPEC => static::PARAM_SPEC_FILE,
-                            static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                            static::PARAM_SLUG_SPEC    => static::PARAM_SPEC_FILE,
+                            static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                         ],
                         static::ACTION_FIELD_PHRASE => [
                             static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_BOOLEAN,
@@ -97,9 +95,8 @@ class Anticaptcha extends DeCaptchaBase
                         static::ACTION_FIELD_MAX_LEN => [
                             static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_INTEGER,
                         ],
-                    ]
+                    ],
                 ],
-
 
 //                static::ACTION_FIELD_QUESTION => [
 //                    static::PARAM_SLUG_DEFAULT => 0,
@@ -118,21 +115,21 @@ class Anticaptcha extends DeCaptchaBase
             ],
         ];
         $this->actions[static::ACTION_UNIVERSAL] = [
-            static::ACTION_URI => 'res.php',
+            static::ACTION_URI    => 'res.php',
             static::ACTION_METHOD => static::ACTION_METHOD_GET,
             static::ACTION_FIELDS => [
                 static::ACTION_FIELD_KEY => [
                     static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_SPEC => static::PARAM_SPEC_API_KEY,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                    static::PARAM_SLUG_SPEC    => static::PARAM_SPEC_API_KEY,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                 ],
                 static::ACTION_FIELD_ACTION => [
                     static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                 ],
                 static::ACTION_FIELD_HEADER_ACAO => [
                     static::PARAM_SLUG_DEFAULT => 0,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_INTEGER,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_INTEGER,
                 ],
                 static::ACTION_FIELD_CAPTCHA_ID => [
                     static::PARAM_SLUG_SPEC => static::PARAM_SPEC_CAPTCHA,
@@ -141,35 +138,35 @@ class Anticaptcha extends DeCaptchaBase
             ],
         ];
         $this->actions[static::ACTION_UNIVERSAL_WITH_CAPTCHA] = [
-            static::ACTION_URI => 'res.php',
+            static::ACTION_URI    => 'res.php',
             static::ACTION_METHOD => static::ACTION_METHOD_GET,
             static::ACTION_FIELDS => [
                 static::ACTION_FIELD_KEY => [
                     static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_SPEC => static::PARAM_SPEC_API_KEY,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                    static::PARAM_SLUG_SPEC    => static::PARAM_SPEC_API_KEY,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_STRING,
                 ],
                 static::ACTION_FIELD_CAPTCHA_ID => [
                     static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_SPEC => static::PARAM_SPEC_CAPTCHA,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_INTEGER,
+                    static::PARAM_SLUG_SPEC    => static::PARAM_SPEC_CAPTCHA,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_INTEGER,
                 ],
                 static::ACTION_FIELD_ACTION => [
-                    static::PARAM_SLUG_REQUIRE => true,
-                    static::PARAM_SLUG_DEFAULT => 'get',
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
+                    static::PARAM_SLUG_REQUIRE  => true,
+                    static::PARAM_SLUG_DEFAULT  => 'get',
+                    static::PARAM_SLUG_TYPE     => static::PARAM_FIELD_TYPE_STRING,
                     static::PARAM_SLUG_VARIABLE => false,
                 ],
                 static::ACTION_FIELD_HEADER_ACAO => [
                     static::PARAM_SLUG_DEFAULT => 0,
-                    static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_INTEGER,
+                    static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_INTEGER,
                 ],
             ],
         ];
 
         $this->decodeSettings[static::DECODE_ACTION][static::DECODE_ACTION_RECOGNIZE] = [
             static::DECODE_SEPARATOR => '|',
-            static::DECODE_PARAMS => [
+            static::DECODE_PARAMS    => [
                 static::DECODE_PARAM_RESPONSE => [
                     static::DECODE_PARAM_SETTING_MARKER => 0,
                 ],
@@ -180,7 +177,7 @@ class Anticaptcha extends DeCaptchaBase
         ];
         $this->decodeSettings[static::DECODE_ACTION][static::DECODE_ACTION_GET] = [
             static::DECODE_SEPARATOR => '|',
-            static::DECODE_PARAMS => [
+            static::DECODE_PARAMS    => [
                 static::DECODE_PARAM_RESPONSE => [
                     static::DECODE_PARAM_SETTING_MARKER => 0,
                 ],
@@ -191,7 +188,7 @@ class Anticaptcha extends DeCaptchaBase
         ];
         $this->decodeSettings[static::DECODE_ACTION][static::DECODE_ACTION_UNIVERSAL] = [
             static::DECODE_SEPARATOR => '|',
-            static::DECODE_PARAMS => [
+            static::DECODE_PARAMS    => [
                 static::DECODE_PARAM_RESPONSE => [
                     static::DECODE_PARAM_SETTING_MARKER => 0,
                 ],
@@ -206,7 +203,7 @@ class Anticaptcha extends DeCaptchaBase
      */
     public function getBalance()
     {
-        return (float)$this->requestUniversal('getbalance')[static::DECODE_PARAM_RESPONSE];
+        return (float) $this->requestUniversal('getbalance')[static::DECODE_PARAM_RESPONSE];
     }
 
     /**
