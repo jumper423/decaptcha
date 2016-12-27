0319cc75cd62f844dc97adad60cabd70f2d0c954
Vlad Pasechnik <pasechnik@playform.com>
2016-12-27 17:08:40 +0300

    Socialink

--- src/services/Socialink.php
+++ src/services/Socialink.php
@@ -3,34 +3,27 @@
 namespace jumper423\decaptcha\services;
 
 /**
- * Class Pixodrom.
+ * Class Socialink.
  */
-class Pixodrom extends RuCaptcha
+class Socialink extends RuCaptcha
 {
-    protected $host = 'pixodrom.com';
+    protected $host = 'socialink.ru';
 
     public function init()
     {
         parent::init();
-
         unset(
-            $this->paramsNames[self::ACTION_FIELD_LANGUAGE],
             $this->paramsNames[self::ACTION_FIELD_QUESTION],
             $this->paramsNames[self::ACTION_FIELD_TEXTINSTRUCTIONS],
             $this->paramsNames[self::ACTION_FIELD_PINGBACK],
-            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_LANGUAGE],
             $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_QUESTION],
             $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_TEXTINSTRUCTIONS],
             $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_PINGBACK]
         );
         $this->paramsNames[self::ACTION_FIELD_IS_RUSSIAN] = 'is_russian';
-        $this->paramsNames[self::ACTION_FIELD_LABEL] = 'label';
         $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_IS_RUSSIAN] = [
             static::PARAM_SLUG_DEFAULT => 0,
             static::PARAM_SLUG_TYPE    => static::PARAM_FIELD_TYPE_INTEGER,
-        ];
-        $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_LABEL] = [
-            static::PARAM_SLUG_TYPE => static::PARAM_FIELD_TYPE_STRING,
         ];
         $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_SOFT_ID][self::PARAM_SLUG_DEFAULT] = 0;
     }
