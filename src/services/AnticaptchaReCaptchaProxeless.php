d36b647a65fa4cb7674abe32701a58bac36ba472
Vlad Pasechnik <pasechnik@playform.com>
2016-12-27 16:56:27 +0300

    unset

--- src/services/AnticaptchaReCaptchaProxeless.php
+++ src/services/AnticaptchaReCaptchaProxeless.php
@@ -11,6 +11,25 @@ class AnticaptchaReCaptchaProxeless extends Anticaptcha
     {
         parent::init();
 
+        unset(
+            $this->paramsNames[self::ACTION_FIELD_FILE],
+            $this->paramsNames[self::ACTION_FIELD_PHRASE],
+            $this->paramsNames[self::ACTION_FIELD_PINGBACK],
+            $this->paramsNames[self::ACTION_FIELD_REGSENSE],
+            $this->paramsNames[self::ACTION_FIELD_NUMERIC],
+            $this->paramsNames[self::ACTION_FIELD_CALC],
+            $this->paramsNames[self::ACTION_FIELD_MIN_LEN],
+            $this->paramsNames[self::ACTION_FIELD_MAX_LEN],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_FILE],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_PHRASE],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_PINGBACK],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_REGSENSE],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_NUMERIC],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_CALC],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_MIN_LEN],
+            $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][self::ACTION_FIELD_MAX_LEN]
+        );
+
         $this->paramsNames[static::ACTION_FIELD_PAGEURL] = 'websiteURL';
         $this->paramsNames[static::ACTION_FIELD_GOOGLEKEY] = 'websiteKey';
         $this->paramsNames[static::ACTION_FIELD_GOOGLETOKEN] = 'websiteSToken';
