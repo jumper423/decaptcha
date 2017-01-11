630626da5ecf62d71291b2aac8385f954b4ffa29
Vlad Pasechnik <pasechnik@playform.com>
2017-01-11 17:29:08 +0300

    duplicate

--- src/services/RuCaptchaGrid.php
+++ src/services/RuCaptchaGrid.php
@@ -12,13 +12,13 @@ class RuCaptchaGrid extends RuCaptchaInstruction
         parent::init();
 
         unset(
-            $this->paramsNames[static::ACTION_FIELD_PHRASE],
+            $this->paramsNames [static::ACTION_FIELD_PHRASE],
             $this->paramsNames[static::ACTION_FIELD_PINGBACK],
             $this->paramsNames[static::ACTION_FIELD_REGSENSE],
             $this->paramsNames[static::ACTION_FIELD_NUMERIC],
             $this->paramsNames[static::ACTION_FIELD_CALC],
             $this->paramsNames[static::ACTION_FIELD_MIN_LEN],
-            $this->paramsNames[static::ACTION_FIELD_MAX_LEN],
+            $this->paramsNames[static::ACTION_FIELD_MAX_LEN] ,
             $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_PHRASE],
             $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_NUMERIC],
             $this->actions[static::ACTION_RECOGNIZE][static::ACTION_FIELDS][static::ACTION_FIELD_PINGBACK],
